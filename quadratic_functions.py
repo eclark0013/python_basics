1e3bc168bd697caf2bbc1b7135a86d468f59315b
eclark0013 <eclark0013@gmail.com>
2025-03-13 15:35:33 -0400

    initial commit

---
+++ quadratic_functions.py
@@ -0,0 +1,13 @@
+# given a function ax^2 + bx + c = 0
+def concavity(a, b, c):
+    if a > 0:
+        print("concave up")
+    elif a < 0:
+        print("concave down")
+    else:
+        print("no concavity")
+
+def analyze(a , b, c):
+    concavity(a, b, c)
+
+analyze(5, 6, 6)
