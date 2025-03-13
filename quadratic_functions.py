b8ce1fde4547454773cf9c66dacca47971f446bb
eclark0013 <eclark0013@gmail.com>
2025-03-13 16:01:08 -0400

    Added discriminat and roots_count functions to quadratic_function analyzer

--- quadratic_functions.py
+++ quadratic_functions.py
@@ -1,15 +1,31 @@
 # given a function ax^2 + bx + c = 0
+def analyze(a, b, c):
+    print(f"Concavity is {concavity(a, b, c)}")
+    print(f"Discriminant is {discriminant_value(a, b, c)}")
+    print(f"Number of roots: {roots_count(a, b, c)}")
+
 def concavity(a, b, c):
     if a > 0:
-        print("concave up")
+        return("up")
     elif a < 0:
-        print("concave down")
+        return("down")
     else:
-        print("no concavity")
+        return("none")
 
-def analyze(a , b, c):
-    concavity(a, b, c)
+def discriminant_value(a, b, c): 
+    return(b**2 - 4*a*c)
+
+def roots_count(a, b, c):
+    disc_value = discriminant_value(a, b, c)
+    if disc_value > 0:
+        return("two real roots")
+    elif disc_value == 0:
+        return("one real roots")
+    else: 
+        return("two imaginary roots")
 
 analyze(5, 6, 6)
+
+
 
 
